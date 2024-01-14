6291b06b212c8853aa53faa64e744450c4312573
Anthony Re <anthonyjosephre@gmail.com>
2024-01-14 14:39:56 -0500

    Added function to read gnuplot data into points

--- lintest.py
+++ lintest.py
@@ -2,9 +2,9 @@
 
    A simple program that accepts a list of data points and computes the slope
    between successive pairs of points.
-
-   TODO: Have this read in files in the 'gnuplot' (tsv) format
 """
+
+import os.path
 
 def computeSlope(a, b):
   """computeSlope is the slope of the line passing through points a and b.
@@ -28,6 +28,53 @@ def computeSlope(a, b):
     raise ZeroDivisionError(a, b)
 
   return (a[1] - b[1]) / (a[0] - b[0])
+
+def readPoints(filename, header=False):
+  """
+  readPoints attempts to parse [filename] and produce a list of ordered pairs.
+  [filename] should be a gnuplot-style text file with optional labels as the
+  first line.
+
+  NOTE: For now I only care about single-variable functions, but this should
+  generalize to multivariate data as well.
+
+  Parameters:
+    filename (string): The path to a file containing data columns representing
+    points. The file should be structured as follows:
+
+    XLABEL  YLABEL  # (OPTIONAL)
+    x1      y1
+    x2      y2
+    x3      y3
+    ...     ...
+
+    where each column is separated by whitespace. This corresponds to the
+    format expected by gnuplot.
+
+    See: http://www.gnuplot.info/docs_4.2/node101.html
+
+    header (boolean): If header is True, then the first line containing the
+    optional data labels will be skipped. This program does not make use of the
+    x and y labels.
+
+  Output:
+    A list of ordered pairs whose x-values correspond to the first column and
+    whose y-values correspond to the second column.
+
+  Raises:
+    FileNotFoundError: If [filename] does not exist.
+    TODO: More validation?
+  """
+  def parseLine(line):
+    return line.split()[:2]
+
+  if not os.path.exists(filename):
+    raise FileNotFoundError(filename)
+
+  with open(filename, 'r') as data:
+    lines = list(data)[1:] if header else list(data)
+
+    return [tuple(parseLine(line)) for line in lines]
 
 def slopes(points):
   """
