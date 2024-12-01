411cea17fa6af96655d834749e3d6ad10e22d12c
Anthony Re <anthonyjosephre@gmail.com>
2024-12-01 14:51:29 -0500

    fixed typo

--- crossproduct/xproduct.py
+++ crossproduct/xproduct.py
@@ -15,8 +15,8 @@ The formula for the cross products of vectors u and v is given by:
 
 or in determinant form:
 
-    |u2 u3|  |u3 u2|  |u1 u2|
-    |v2 v3|, |v3 v2|, |v1 v2|
+    |u2 u3|  |u3 u1|  |u1 u2|
+    |v2 v3|, |v3 v1|, |v1 v2|
 """
 
 import sys
